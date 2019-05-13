a4f92fda3064407261845f98739f03e045600107
RSoXS <egann@bnl.gov>
2019-05-13 16:03:45 -0400

    starting locations methods

--- rsoxs_database/locations.py
+++ rsoxs_database/locations.py
@@ -1,9 +1,16 @@
 
+from operator import attrgetter
+import collections
+
 def goto_location(location_id):
-    loc_dict = dict_from_key("location",location_id)
+    loc_dict = dict_from_key("location",location_id) # we need this function
     loc_list = [].append(loc_dict['location_list'])
-    mots = [d['motor'] for d in loc_list if 'motor' in d]
-    sps = [d['setpoint'] for d in loc_list if 'setpoint' in d]
-    order = [d['order'] for d in loc_list if 'order' in d]
+    loc_list = sorted(loc_list,key=attrgetter('order'))
+    orderlist = [o for o in collections.Counter(d['order'] for d in loc_list).keys()]
+    for order in orderlist:
+        outputlist = [[items['motor'], items['position']] for items in loc_list if items['order'] == order]
+        flat_list = [item for sublist in outputlist for item in sublist]
+        yield from bps.mv(*flat_list)
+
 
 
