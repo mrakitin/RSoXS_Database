b4522da805b4ba09f40c5138c801e61120dc18d8
RSoXS <egann@bnl.gov>
2019-05-13 12:45:37 -0400

    added priorities

--- rsoxs_database/Build_and_verify.py
+++ rsoxs_database/Build_and_verify.py
@@ -18,7 +18,7 @@ def load_schema(filename):
 
 
 user_schema = load_schema('RSoXS_User.json')
-acuisition_schema = load_schema('RSoXS_Acquisition.json')
+acquisition_schema = load_schema('RSoXS_Acquisition.json')
 institution_schema = load_schema('RSoXS_Institution.json')
 holder_schema = load_schema('RSoXS_Holder.json')
 location_schema = load_schema('RSoXS_Location.json')
@@ -28,22 +28,25 @@ sample_schema = load_schema('RSoXS_Sample.json')
 def build_and_verify_user(*,date_list=[],email='', first_name='User', institution_id=0,
                           last_checkin=datetime.now().strftime('%G-%m-%d-%I:%M:%S %p'), last_name='McUserface',
                           notes='', past_institutions=[], past_proposals=[], phone='555-555-5555',
-                          proposal_id=0, user_id=0, username='CantFollowDirections', history=[]):
+                          proposal_id=0, user_id=0, username='CantFollowDirections', history=[], priority=0):
 
-    user = {'date_checkin_list': date_list,
-            'email': email,
-            'first_name': first_name,
-            'institution_id': institution_id,
-            'last_checkin': last_checkin,
-            'last_name': last_name,
-            'notes': notes,
-            'past_institutions': past_institutions,
-            'past_proposals': past_proposals,
-            'phone': phone,
-            'proposal_id': proposal_id,
-            'user_id': user_id,
-            'username': username,
-            'history': history}
+    user = {
+        'date_checkin_list': date_list,
+        'email': email,
+        'first_name': first_name,
+        'institution_id': institution_id,
+        'last_checkin': last_checkin,
+        'last_name': last_name,
+        'notes': notes,
+        'past_institutions': past_institutions,
+        'past_proposals': past_proposals,
+        'phone': phone,
+        'proposal_id': proposal_id,
+        'user_id': user_id,
+        'username': username,
+        'history': history,
+        'priority': priority
+    }
 
     jsonschema.validate(user,user_schema)
 
@@ -54,54 +57,59 @@ def build_and_verify_sample(*, sample_name='test', sample_desc='testing',
                             date_created=datetime.now().strftime('%G-%m-%d-%I:%M:%S %p'), user_id=0,
                             project_name='testing', institution_id=0, composition=[], density=1, thickness=1,
                             notes='', state='fresh', current_bar_id=0, current_slot_name='blank',
-                            past_bar_ids=[], location_id=0, requests=[], history=[]):
+                            past_bar_ids=[], location_id=0, requests=[], history=[],priority=0):
 
-    sample = {"sample_name": sample_name,
-              "sample_desc": sample_desc,
-              "date_created": date_created,
-              "user_id": user_id,
-              "project_name": project_name,
-              "institution_id": institution_id,
-              "composition": composition,
-              "density": density,
-              "thickness": thickness,
-              "notes": notes,
-              "state": state,
-              "current_bar_id": current_bar_id,
-              "current_slot_name": current_slot_name,
-              "past_bar_ids": past_bar_ids,
-              "location_id": location_id,
-              "requests": requests,
-              "history": history
-              }
+    sample = {
+        "sample_name": sample_name,
+        "sample_desc": sample_desc,
+        "date_created": date_created,
+        "user_id": user_id,
+        "project_name": project_name,
+        "institution_id": institution_id,
+        "composition": composition,
+        "density": density,
+        "thickness": thickness,
+        "notes": notes,
+        "state": state,
+        "current_bar_id": current_bar_id,
+        "current_slot_name": current_slot_name,
+        "past_bar_ids": past_bar_ids,
+        "location_id": location_id,
+        "requests": requests,
+        "history": history,
+        "priority": priority
+    }
 
     jsonschema.validate(sample,sample_schema)
     return sample
 
 
 def build_and_verify_location(*,location_list=[],display='private', favorite=False,
-                              creator_id=0, sample=False, config=False):
+                              creator_id=0, sample=False, config=False, priority=0):
 
-    location = {"location_list": location_list,
-                "display": display,
-                "favorite": favorite,
-                "creator_ID": creator_id,
-                "sample": sample,
-                "config": config
-                }
+    location = {
+        "location_list": location_list,
+        "display": display,
+        "favorite": favorite,
+        "creator_ID": creator_id,
+        "sample": sample,
+        "config": config,
+        "priority": priority
+    }
 
     jsonschema.validate(location,location_schema)
     return location
 
 
 def build_and_verify_institution(*,institution_id=0, full_name='School of Hard Knocks', short_name='the_street',
-                                 notes=''):
+                                 notes='', priority=0):
 
     institution = {
         "institution_id": institution_id,
         "full_name": full_name,
         "short_name": short_name,
-        "notes": notes
+        "notes": notes,
+        "priority": priority
     }
 
     jsonschema.validate(institution,institution_schema)
@@ -129,7 +137,7 @@ def build_and_verify_holder(*, holder_id=0, holder_name='Default', primary_user_
 
 
 def build_and_verify_acquisition(*,plan_name='none',detectors=[], motors=[], positions=[], display='private',
-                                 favorite=False, creator_id=0):
+                                 favorite=False, creator_id=0, priority=0):
 
     acquisition = {
         "plan_name": plan_name,
@@ -138,8 +146,10 @@ def build_and_verify_acquisition(*,plan_name='none',detectors=[], motors=[], pos
         "positions": positions,
         "display": display,
         "favorite": favorite,
-        "creator_id": creator_id
+        "creator_id": creator_id,
+        "priority": priority
     }
 
     jsonschema.validate(acquisition,acquisition_schema)
     return acquisition
+
