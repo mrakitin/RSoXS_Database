4f4e4cde8ee242d562d1940dac645a14faddf9f6
RSoXS <egann@bnl.gov>
2019-05-23 14:05:07 -0400

    starting acquisitions

--- rsoxs_database/acquisitions.py
+++ rsoxs_database/acquisitions.py
@@ -1,6 +1,8 @@
 import numpy as np
+from cycler import cycler
+from bluesky.plans import scan_nd
 
-def SWCarbon_acq(multiple):
+def SWCarbon_acq(multiple,mesh,det,en):
     energies = np.arange(270,282,.5)
     energies = np.append(energies,np.arange(282,286,.1))
     energies = np.append(energies,np.arange(286,292,.2))
@@ -12,5 +14,17 @@ def SWCarbon_acq(multiple):
     times[(energies < 286) & (energies >= 282)] = 5
     times[energies >= 286] = 2
     times *= multiple
+   # print(times)
+   # print(energies)
+   # print(len(times))
+   # print(len(energies))
+    times2 = times.copy()
+   # print(len(times2))
+
+    yield from scan_nd(
+        [mesh,det],
+        cycler(det.saxs.cam.acquire_time, times) +
+        cycler(mesh.parent.exposure_time, times2) +
+        cycler(en, energies))
 
 
